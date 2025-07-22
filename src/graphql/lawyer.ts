d30fa035461cb3290290c909078c835c33211deb
25LP9675 25LP9675 <25LP9675@admins-iMac-2.local>
2025-07-22 20:57:11 +0800

    done

--- src/graphql/lawyer.ts
+++ src/graphql/lawyer.ts
@@ -13,3 +13,91 @@ export const CREATE_LAWYER_MUTATION = gql`
     }
   }
 `;
+export const GET_LAWYER_BY_ID_QUERY = gql`
+  query GetLawyerById($lawyerId: ID!) {
+    getLawyerById(lawyerId: $lawyerId) {
+      _id
+      lawyerId
+      clerkUserId
+      clientId
+      firstName
+      lastName
+      email
+      licenseNumber
+      bio
+      university
+      achievements {
+        _id
+        title
+        description
+        threshold
+        icon
+      }
+      status
+      document
+      rating
+      profilePicture
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const GET_LAWYER_BY_LAWYERID_QUERY = gql`
+  query GetLawyerById($lawyerId: ID!) {
+    getLawyerById(lawyerId: $lawyerId) {
+      _id
+      lawyerId
+      clerkUserId
+      clientId
+      firstName
+      lastName
+      email
+      licenseNumber
+      bio
+      university
+      achievements {
+        _id
+        title
+        description
+        threshold
+        icon
+      }
+      status
+      document
+      rating
+      profilePicture
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const UPDATE_LAWYER_MUTATION = gql`
+  mutation Mutation($updateLawyerLawyerId2: ID!, $input: UpdateLawyerInput!) {
+    updateLawyer(lawyerId: $updateLawyerLawyerId2, input: $input) {
+      _id
+      lawyerId
+      clerkUserId
+      clientId
+      firstName
+      lastName
+      email
+      licenseNumber
+      bio
+      university
+      achievements {
+        _id
+        title
+        description
+        threshold
+        icon
+      }
+      status
+      document
+      rating
+      profilePicture
+      createdAt
+      updatedAt
+    }
+  }
+`;
